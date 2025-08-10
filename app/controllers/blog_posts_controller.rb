023429df4889c9814113922d24c6d1bae61746e2
Ivan Hrekov <iandrc9@gmail.com>
2025-08-10 14:18:59 +0200

    refactor: add before_action to controller

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,12 +1,12 @@
 class BlogPostsController < ApplicationController
+  before_action :set_blog_post, except: [ :index, :new, :create ]
+
   def index
     @blog_posts = BlogPost.all
   end
 
   def show
-    @blog_post = BlogPost.find(params[:id])
-    rescue ActiveRecord::RecordNotFound
-      redirect_to root_path
+
   end
 
   def new
@@ -23,11 +23,9 @@ class BlogPostsController < ApplicationController
   end
 
   def edit
-    @blog_post = BlogPost.find(params[:id])
   end
 
   def update
-    @blog_post = BlogPost.find(params[:id])
     if @blog_post.update(blog_post_params)
       redirect_to @blog_post
     else
@@ -36,7 +34,6 @@ class BlogPostsController < ApplicationController
   end
 
   def destroy
-    @blog_post = BlogPost.find(params[:id])
     @blog_post.destroy
     redirect_to root_path
   end
@@ -45,5 +42,11 @@ class BlogPostsController < ApplicationController
 
   def blog_post_params
     params.require(:blog_post).permit(:title, :body)
+  end
+
+  def set_blog_post
+    @blog_post = BlogPost.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path
   end
 end
